881012d98177f111cc935762289787e11e0bc01c
nguyenvanduocit <nguyenvanduocit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-17 03:50:05 +0000

    INIT
    git-svn-id: https://plugins.svn.wordpress.org/wp-vn-oembed/trunk@991578 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Wordpress VN oEmbed
+Version: 1.0.0
+Description: Hỗ trợ oEmbed cho các website chia sẻ nhạc ở Việt Nam
+Author: Vô Minh
+Plugin URI: http://laptrinh.senviet.org
+*/
+
+include dirname(__FILE__) . '/scb/load.php';
+include_once dirname(__FILE__) . '/core.php';
+function _wpvne_init()
+{
+    Wpos_Code::instance()->init();
+}
+
+scb_init('_wpvne_init');
